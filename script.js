8b73e9a1ac0cacb9151e6583b4827c284add5ca5
Hossein Hajipour <hossein82hajipour@gmail.com>
2025-09-13 19:34:44 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function b()
+{
+    const d = document.getElementById('d5') ;
+    if( d.style.display === 'none')
+    {
+        d.style.display = 'block'
+    }
+    else
+    {
+        d.style.display = 'none'
+    }
+}
